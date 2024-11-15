b1c14280e8ae52d708e7b4d9075aa3a94fddc105
Alex Clifton <alex@clifton.org.uk>
2024-11-15 04:33:35 +0000

    Login, signup, and logout functionality now working.

---
+++ login_database.py
@@ -0,0 +1,59 @@
+import sqlite3
+import os
+import threading
+import werkzeug.security
+import uuid
+from user import User
+
+
+def hash_password(password):
+    return werkzeug.security.generate_password_hash(password, 'pbkdf2:sha256')
+
+
+def verify_password(user, password):
+    return werkzeug.security.check_password_hash(user.password, password)
+
+
+class LoginDatabase:
+    database_path = "logins.db"
+    database_lock = threading.Lock()
+
+    def __init__(self):
+        database_exists = os.path.isfile(self.database_path)
+        self.connection = sqlite3.connect(self.database_path, check_same_thread=False)
+        if not database_exists:
+            self.connection.execute("""CREATE TABLE Logins (
+                                    Id	TEXT,
+                                    Email	TEXT,
+                                    Password	TEXT,
+                                    PRIMARY KEY(Id)
+                                );""")
+
+    def fetch_user(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute("""SELECT * FROM Logins WHERE Id = (?)""", (user_id, ))
+        item = cursor.fetchone()
+        return None if item is None else User(item[0], item[1], item[2])
+
+    def fetch_user_by_email(self, email):
+        cursor = self.connection.cursor()
+        cursor.execute("""SELECT * FROM Logins WHERE Email = (?)""", (email, ))
+        item = cursor.fetchone()
+        return None if item is None else User(item[0], item[1], item[2])
+
+    def add_user(self, email, password):
+        hashed_password = hash_password(password)
+        user_id = str(uuid.uuid4())
+        while not self.check_unique_user_id(user_id):
+            user_id = str(uuid.uuid4())
+        self.database_lock.acquire()
+        self.connection.execute("""INSERT INTO Logins VALUES (?, ?, ?)""", (user_id, email, hashed_password))
+        self.connection.commit()
+        self.database_lock.release()
+
+    def check_unique_user_id(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute("""SELECT * FROM Logins WHERE Id = (?)""", (user_id,))
+        item = cursor.fetchone()
+        return item is None
+
--- main.py
+++ main.py
@@ -1,13 +1,83 @@
 import flask
-from database import Database
+import flask_login
+import database
+import login_database
+from user import User
 
-app = flask.Flask(__name__)
-database = Database()
+app = flask.Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = 'iBuzz2FourthYearGroupDevelopmentProject'
+db = database.Database()
+login_db = login_database.LoginDatabase()
+login_manager = flask_login.LoginManager(app)
+login_manager.login_view = "login"
+
+
+@app.route('/', methods=['GET'])
+def redirect_user():
+    return flask.redirect('/login')
 
 
 @app.route('/', methods=['POST'])
 def receive_json():
     json = flask.request.json
     print(json)
-    database.data_received(json)
+    db.data_received(json)
     return '', 200
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return login_db.fetch_user(user_id)
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    return flask.render_template('login/login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = flask.request.form.get('email')
+    password = flask.request.form.get('password')
+    remember = flask.request.form.get('remember')
+    user = login_db.fetch_user_by_email(email)
+    if login_database.verify_password(user, password):
+        flask_login.login_user(user, remember=bool(remember))
+        return flask.redirect('/dashboard')
+
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    return flask.render_template('signup/signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    email = flask.request.form.get('email')
+    print(email)
+    if login_db.fetch_user_by_email(email) is not None:
+        return flask.abort(400)
+    password = flask.request.form.get('password')
+    password_repeat = flask.request.form.get('password_repeat')
+    if password != password_repeat:
+        return flask.abort(400)
+    login_db.add_user(email, password)
+    return flask.redirect('/login')
+
+
+@app.route('/logout', methods=['GET'])
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return flask.redirect('/login')
+
+
+@app.route('/dashboard', methods=['GET'])
+@flask_login.login_required
+def dashboard():
+    return flask.render_template('dashboard/dashboard.html')
+
+
+@app.route('/templates/<path:path>')
+def send_report(path):
+    return flask.send_from_directory('templates', path)
