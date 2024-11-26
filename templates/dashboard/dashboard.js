5ec5a636522a713d0c52a2e744a24a232cb163e2
Alex Clifton <alex@clifton.org.uk>
2024-11-26 14:39:19 +0000

    Dashboard now plots Outside_temp with automatic axis

--- templates/dashboard/dashboard.js
+++ templates/dashboard/dashboard.js
@@ -1,11 +1,54 @@
 /* globals Chart:false */
 
-(() => {
-    'use strict'
+let fetched_data = {}
 
-    // Graphs
+async function get_data(field) {
+    fetch("/data/" + field).then((r) => {
+        if (!r.ok) {
+            throw new Error("Bad Request")
+        }
+        return r.json()
+    }).then((json) => {
+        console.log(json)
+        let times = json['Time']
+        let data = json[field]
+        let reformatted = []
+        for (let i = 0; i < times.length; i++) {
+            reformatted.push({t:times[i], y:data[i]})
+        }
+        console.log(reformatted)
+        fetched_data[field] = reformatted
+    })
+}
+
+function make_chart(field) {
+    get_data(field).then(() => {
+        const ctx = document.getElementById('myChart')
+        let myChart = new Chart(ctx, {
+            type: 'line',
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                    }
+                }
+            },
+            data: {
+                datasets: [{
+                    label: field,
+                    data: fetched_data[field],
+                }]
+            }
+        })
+    })
+}
+
+make_chart('Outside_temp')
+
+function main() {
+// Graphs
     const ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
     const myChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -46,4 +89,5 @@
             }
         }
     })
-})()
+}
+
