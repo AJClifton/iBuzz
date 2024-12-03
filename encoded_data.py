ede2fa8416c1e472b379a57cd0f346a9718cadbd
Alex Clifton <alex@clifton.org.uk>
2024-12-03 16:12:45 +0000

    Added debug print messages for received data

--- encoded_data.py
+++ encoded_data.py
@@ -2,7 +2,8 @@ import base64
 
 
 def extract_ela_rht_data(data):
-    extracted_data = extract_data(data, [6, 12, 3, 1, 2], [False, False, False, True, True], [False, False, False, False, True])
+    extracted_data = extract_data(data, [6, 12, 3, 1, 2], [False, False, False, True, True],
+                                  [False, False, False, False, True])
     return extracted_data[0], extracted_data[1] / 100
 
 
@@ -11,7 +12,10 @@ def extract_data(data, sizes, to_return, is_reversed):
     hex_data = base64.b64decode(data).hex()
     print('hex: ' + hex_data)
     extracted_data = []
-    print('ASCII: ' + bytearray.fromhex(hex_data).decode())
+    try:
+        print('ASCII: ' + bytearray.fromhex(hex_data).decode('utf-8'))
+    except Exception as e:
+        print('ASCII: ' + bytearray.fromhex(hex_data[:8]).decode('utf-8'))
     for i in range(len(sizes)):
         section, hex_data = hex_data[:sizes[i] * 2], hex_data[sizes[i] * 2:]
         if not to_return[i]:
