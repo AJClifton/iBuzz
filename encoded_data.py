eb4feb68d1b82afe256ff053c8622d144b388809
Alex Clifton <alex@clifton.org.uk>
2024-12-03 15:57:47 +0000

    Added debug print messages for received data

--- encoded_data.py
+++ encoded_data.py
@@ -7,8 +7,11 @@ def extract_ela_rht_data(data):
 
 
 def extract_data(data, sizes, to_return, is_reversed):
+    print('base64: ' + data)
     hex_data = base64.b64decode(data).hex()
+    print('hex: ' + hex_data)
     extracted_data = []
+    print('ASCII: ' + bytearray.fromhex(hex_data).decode())
     for i in range(len(sizes)):
         section, hex_data = hex_data[:sizes[i] * 2], hex_data[sizes[i] * 2:]
         if not to_return[i]:
@@ -16,4 +19,5 @@ def extract_data(data, sizes, to_return, is_reversed):
         if is_reversed[i]:
             section = "".join(list(section[0 + i:2 + i] for i in range(0, len(section), 2))[::-1])
         extracted_data.append(int(section, 16))
+    print('ELA Extracted: ' + str(extracted_data))
     return extracted_data
